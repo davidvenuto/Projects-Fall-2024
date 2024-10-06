23eef01f84f65916c6b7cb1aeb4fd90e262cf672
davidvenuto <135266125+davidvenuto@users.noreply.github.com>
2024-10-05 20:44:05 -0400

    Created server.js

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router'; 
+import router from './router';
+import GoogleSignInPlugin from 'vue3-google-login'; 
 
 const app = createApp(App);
+
 app.use(router);
+app.use(GoogleSignInPlugin, {
+  clientId: '748775701811-crnrqbspef48i5qtclsjua2qrf1n2kkt.apps.googleusercontent.com', // Replace with your actual client ID
+});
+
 app.mount('#app');
